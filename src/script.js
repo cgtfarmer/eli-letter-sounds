93608f4a9ed59b6ada38c54a5d58605cbf2d378f
Christian Farmer <christian@taxstrategistsofamerica.com>
2026-03-07 19:57:42 -0600

    Updates

--- src/script.js
+++ src/script.js
@@ -1,9 +1,101 @@
+const optionsElement = document.querySelector('#options');
+const displayElement = document.querySelector('#display');
 
-var nameInput = document.querySelector("#name-input");
-var greetingOutput = document.querySelector("#greeting-output");
+const letters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
 
-function doTheThing() {
-  var name = nameInput.value;
+const problemLetters = [
+  'a',
+  'b',
+  'd',
+  't',
+  'i',
+  'l',
+  'g',
+  'j',
+  'q'
+]
 
-  greetingOutput.innerHTML = "Hello " + name + ", welcome home!";
+function main() {
+  letters.forEach((letter) => {
+    optionsElement.insertAdjacentHTML('beforeend', `
+      <input type="checkbox" id="${letter}" checked="true">${letter}</input><br/>
+    `);
+  });
 }
+
+function toggleOptions() {
+  optionsElement.hidden = !optionsElement.hidden;
+}
+
+function selectAll() {
+  letters.forEach((letter) => {
+    document.querySelector(`#${letter}`).checked = true
+  });
+}
+
+function deselectAll() {
+  letters.forEach((letter) => {
+    document.querySelector(`#${letter}`).checked = false
+  });
+}
+
+function challenging() {
+  letters.forEach((letter) => {
+    document.querySelector(`#${letter}`).checked =
+      problemLetters.includes(letter);
+  });
+}
+
+function draw() {
+  selectedLetters = [];
+
+  letters.forEach((letter) => {
+    if (document.querySelector(`#${letter}`).checked != true) return;
+
+    selectedLetters.push(letter);
+  });
+
+  console.log(selectedLetters);
+
+  const value = getRandomIntInclusive(0, selectedLetters.length - 1);
+
+  displayElement.innerHTML = '';
+
+  window.setTimeout(() => {
+    displayElement.innerHTML = selectedLetters[value]
+  }, 200);
+}
+
+function getRandomIntInclusive(min, max) {
+  // min = inclusive, max = inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+main();
